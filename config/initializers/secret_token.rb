1d3b4d77e4b51dd3b6a3e7a65b8bd37c8bfdc7be
pizad <howtolegal@gmail.com>
2014-01-07 21:32:53 -0600

    Spree_Clean_Install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jfinnley::Application.config.secret_key_base = 'ca4b2b7dfe3338a25bb95ab9da892f22a268151133774373978dd6cd2b021fdd1b94bf2d1118c42307b4bbd8883f33e3dcfdbbe1b1244d27728ddbb32a9bbc47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jfinnley::Application.config.session_store :cookie_store, key: '_jfinnley_session'
